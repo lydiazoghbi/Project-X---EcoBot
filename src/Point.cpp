58266441e30c7055852fbf70c721c3f1182bb536
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2019-11-25 11:25:38 -0500

    Task 2.8 - [Point.cpp] added Doxygen comments and license

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,16 +1,43 @@
+ /*
+  *
+  * Copyright 2019 Lydia Zoghbi and Ryan Bates.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
+/**
+ *  @file       Point.cpp
+ *  @author     Lydia Zoghbi
+ *  @copyright  Copyright Apache 2.0 License
+ *  @date       11/25/2019
+ *  @version    1.0
+ *
+ *  @brief      Implementation class of Point class
+ *
+ */
+
 #ifndef INCLUDE_POINT_HPP_
 #define INCLUDE_POINT_HPP_
 
-class Point {
-	private:
-		double x, y;
+// Constructor for the class Point
+explicit Point::Point(double startX, double startY)
 
-	public:
-		explicit Point(double startX, double startY);
+// Function for extracting the x position from the class
+double Point::getX()
 
-		double getX();
+// Function for extracting the y position from the class
+double Point::getY()
 
-		double getY();
 }
 
 #endif //  INCLUDE_POINT_HPP
