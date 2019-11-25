94324621053c76fe787f1e2a135a944eddcac9dc
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2019-11-25 11:26:06 -0500

    Task 2.8 - [VelocityGenerator.cpp] added Doxygen comments and license

--- src/VelocityGenerator.cpp
+++ src/VelocityGenerator.cpp
@@ -1,3 +1,31 @@
+ /*
+  *
+  * Copyright 2019 Lydia Zoghbi and Ryan Bates.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
+/**
+ *  @file       Point.cpp
+ *  @author     Lydia Zoghbi
+ *  @copyright  Copyright Apache 2.0 License
+ *  @date       11/25/2019
+ *  @version    1.0
+ *
+ *  @brief      Implementation class of VelocityGenerator class
+ *
+ */
+
 #ifndef INCLUDE_VELOCITYGENERATOR_HPP_
 #define INCLUDE_VELOCITYGENERATOR_HPP_
 
@@ -6,20 +34,17 @@
 #include <DebrisCollection.hpp>
 #include <Point.hpp>
 
-class VelocityGenerator {
+// Constructor for the class
+VelocityGenerator::VelocityGenerator()
 
-	private:
-		double xVelocity;
-		double yVelocity;
-		double turnVelocity;
-		Point binLocation;
-		DebrisCollection debrisCollection;
+// Function for finding velocity commands given a desired goal position
+VelocityGenerator::computeFK(Point desiredPoint)
 
-	public:
-		VelocityGenerator();
-		computeFK(Point desiredPoint);
-		positionCallback();
-		orientationCallback(const tf &);
+// Callback function to get robot's current position
+VelocityGenerator::positionCallback();
+
+// Callback function to get robot's current orientation
+VelocityGenerator::orientationCallback(const tf &);
 }
 
 #endif //  INCLUDE_VELOCITYGENERATOR_HPP
