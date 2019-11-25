392e1e36196d94abbc7a0f350b25519783d4d3f6
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2019-11-25 11:25:52 -0500

    Task 2.8 - [DebrisCollection.cpp] added Doxygen comments and license

--- src/DebrisCollection.cpp
+++ src/DebrisCollection.cpp
@@ -1,3 +1,31 @@
+ /*
+  *
+  * Copyright 2019 Lydia Zoghbi and Ryan Bates.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
+/**
+ *  @file       DebrisCollection.cpp
+ *  @author     Lydia Zoghbi
+ *  @copyright  Copyright Apache 2.0 License
+ *  @date       11/25/2019
+ *  @version    1.0
+ *
+ *  @brief      Implementation class of DebrisCollection class
+ *
+ */
+
 #ifndef INCLUDE_DEBRISCOLLECTION_HPP_
 #define INCLUDE_DEBRISCOLLECTION_HPP_
 
@@ -5,25 +33,28 @@
 #include <Point.hpp>
 #include <opencv3/opencv.hpp>
 
+// Reading image from the robot's camera
+DebrisCollection::imageRGBCallback(const sensor_msgs:::CompressedImage &)
 
-class DebrisCollection {
+// Reading depth information from the robot's camera
+DebrisCollection::DepthCallback(const sensor_msgs::Image &)
 
-	private:
-		cv::Mat rgbImage;
-		double hueThreshold;
-		double valueThreshold;
-		double saturationThreshold;
-		std::vector<Point> debrisLocation;
+// Class constructor
+DebrisCollection::DebrisCollection()
 
-	public:
-		imageRGBCallback(const sensor_msgs:::CompressedImage &);
-		image DepthCallback(const sensor_msgs::Image &);
-		DebrisCollection();
-		cv::Mat Filter();
-		Point detectDebris(cv::Mat filteredImage);
-		addDebris(Point detectedDebris);
-		removeDebris();
-		std::vector<Point> sortDebrisLocation(std::vector<Point> * debrisLocations);
-}
+// Applying HSV filter to detect debrid
+cv::Mat DebrisCollection::Filter()
+
+// Function for detecting debris after applying filter
+Point DebrisCollection::detectDebris(cv::Mat filteredImage)
+
+// Function for concatenating debris information if detected
+DebrisCollection::addDebris(Point detectedDebris)
+
+// Function for removing debris from list after it is scooped
+DebrisCollection::removeDebris()
+
+// Sorting the debris by closest to bin 
+std::vector<Point> DebrisCollection::sortDebrisLocation(std::vector<Point> * debrisLocations)
 
 #endif //  INCLUDE_DEBRISCOLLECTION_HPP
