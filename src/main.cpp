760a17bca25a5de9aabc7831d5b3532f57f2777e
rjb3vp <rjb3vp@umd.edu>
2019-11-30 22:27:34 -0500

    Task 2.09 - [main.cpp] main starts node now

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 #include "Point.hpp"
 #include "DebrisCollection.hpp"
 #include "VelocityGenerator.hpp"
+#include <cv_bridge/cv_bridge.h>
+#include "ros/ros.h"
 
 int main(int argc, char **argv) {
-	return 0;
+ 
+  ros::init(argc, argv, "DebrisCollection");
+  ros::NodeHandle nh;
+  DebrisCollection debrisCollection;
+  ROS_INFO_STREAM("Node initialized.");
+  return 0;
 }
