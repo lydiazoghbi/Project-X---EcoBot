008064f92871e86a71a3a54abdd48324074e4175
lydiazoghbi <lalzogbi@teprmail.umd.edu>
2019-12-07 18:49:23 -0500

    Task 3.03 - [StateMachine.hpp][ImageAnalysis.hpp] added new class, renamed old one

--- include/StateMachine.hpp
+++ include/StateMachine.hpp
@@ -16,18 +16,18 @@
   *
   */
 /**
- *  @file       DebrisCollection.hpp
+ *  @file       StateMachine.hpp
  *  @author     Lydia Zoghbi
  *  @copyright  Copyright Apache 2.0 License
  *  @date       12/05/2019
  *  @version    1.0
  *
- *  @brief      Header file for DebrisCollection class
+ *  @brief      Header file for StateMachine class
  *
  */
 
-#ifndef INCLUDE_DEBRISCOLLECTION_HPP_
-#define INCLUDE_DEBRISCOLLECTION_HPP_
+#ifndef INCLUDE_STATEMACHINE_HPP_
+#define INCLUDE_STATEMACHINE_HPP_
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
@@ -46,38 +46,26 @@
 #include <opencv2/imgcodecs/imgcodecs.hpp>
 
 #include "Point.hpp"
+#include "ImageAnalysis.hpp"
 
 /**
- *  @brief      Elements and members of DebrisCollection class
+ *  @brief      Elements and members of StateMachine class
  */
 
-class DebrisCollection {
+class StateMachine {
 
 	private:
-		// State 0 is searching for debris, state 1 found debris, state 2 going to disposal bin
-		int state = 0;
+		ImageAnalysis imageAnalysis;
 
 		// Storage Point for debris position in pixels
 		Point imageDebrisLocation;
 		
 		double orientation;
-		double distanceTraveled;
-
 
 		double depth;
-		double registeredDepth;
 
 		double x;
 		double y;
-
-		// Storage Point for debris position i world frame (orientation, distance)
-		//Point currentDebrisLocation;
-
-
-		int proximityThreshold = 300;
-
-		// Storage vector for all debris locations
-		std::vector<Point> debrisLocation;
 
 		// Image storage
 		cv::Mat lastSnapshot;
@@ -89,11 +77,10 @@ class DebrisCollection {
 		ros::Publisher pub;
 
 		// Defining node subscriber for RGB image callback
-		ros::Subscriber sub;
+		ros::Subscriber imgSub;
 		// Defining node subscriber for odometry callback
                 ros::Subscriber odomSub;
-		// Defining node subscriber for RGB-D depth callback
-		image_transport::Subscriber depthSub;
+
 
 	public:
 
@@ -102,21 +89,7 @@ class DebrisCollection {
  		*  @param      None
  		*  @return     None
  		*/
-		DebrisCollection();
-
-		/**
- 		*  @brief      Constructor for Image setter
- 		*  @param      RGB image
- 		*  @return     None
- 		*/
-		void setImage(cv::Mat image);
-
-		/**
- 		*  @brief      Getter for Image setter
- 		*  @param      None
- 		*  @return     RGB image
- 		*/
-		cv::Mat getImage();
+		StateMachine();
 
 		/**
  		*  @brief      Callback function to obtain the images
@@ -138,42 +111,7 @@ class DebrisCollection {
  		*  @param      A ros message which is the depth reading
  		*  @return     Depth of selected point in image, nothing explicit
  		*/
-		void DepthCallback(const sensor_msgs::ImageConstPtr& depthMessage);
-
-		/**
- 		*  @brief      Function for applying filter to an RGB image
- 		*  @param      An RGB image
- 		*  @return     A black and white filtered image, white being for the debris
- 		*/
-		cv::Mat filter(cv::Mat rawImage);
-
-		/**
- 		*  @brief      Function for detecting the centroid of the debris
- 		*  @param      The black and white filtered image
- 		*  @return     An (x,y) position for the detected debris, if any
- 		*/
-		void detectDebris(cv::Mat filteredImage);
-
-		/**
- 		*  @brief      Function for concatenating debris into a vector if detected
- 		*  @param      The location of a detected debris
- 		*  @return     None
- 		*/
-		void addDebris(Point detectedDebris);
-
-		/**
- 		*  @brief      Function for removing debris from list after being scooped
- 		*  @param      None
- 		*  @return     None
- 		*/
-		void removeDebris();
-
-		/**
- 		*  @brief      Function for sorting debris in the Debris vector
- 		*  @param      Debris locations in a vector
- 		*  @return     Sorted debris locations in a vector
- 		*/
-		std::vector<Point> sortDebrisLocation(std::vector<Point> * debrisLocations);
+		void depthCallback(const sensor_msgs::ImageConstPtr& depthMessage);
 
 		/**
  		*  @brief      Function for obtaining depth information at certain image pixel position
@@ -182,13 +120,10 @@ class DebrisCollection {
  		*  @param      Depth image from RGB-D camera
  		*  @return     Depth of specified pixel on image
  		*/
-		double ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image);
+		double readDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image);
 
 		void pickupDebris();
 
-		void dumpDebris(Point positionOfReceptacle);
-
-		double goToBin(double currentOrientation, double currentDistance);
 };
 
-#endif  // INCLUDE_DEBRISCOLLECTION_HPP_
+#endif  // INCLUDE_STATEMACHINE_HPP_
